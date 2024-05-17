988dec8dc4d8f97952dc0da3476645e0bd67c9d4
Mehdi Labbafi <160803985+MehdiLabbafi@users.noreply.github.com>
2024-05-16 20:52:15 -0400

    Update MagicStaff.java
    edit

--- Mehdi_Java_Assignment_4/weapon/MagicStaff.java
+++ Mehdi_Java_Assignment_4/weapon/MagicStaff.java
@@ -1,19 +1,19 @@
 package weapon;
 
 public class MagicStaff extends Weapon {
-    private int damageAmount = 200; // مقدار آسیب
+    private int damageAmount = 200; // The amount of damage
 
     public MagicStaff() {
         super();
         super.setDamageAmount(damageAmount);
-        super.setDexterityCost(150); // هزینه حرکتی
+        super.setDexterityCost(150); // Movement cost
         super.setWeaponType("Magic Staff");
     }
 
     @Override
     public int strike(int weatherIntensity, int attackType, int strength, int dexterity) {
         int amount = 0;
-        // منطق حمله برای MagicStaff را اینجا اضافه کنید
+        // Add attack logic for MagicStaff here
         return amount;
     }
 }
