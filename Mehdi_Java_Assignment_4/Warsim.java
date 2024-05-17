1ae2c08a52ff9114b0d67098e7e92fb6a1e48ff3
Mehdi Labbafi <160803985+MehdiLabbafi@users.noreply.github.com>
2024-05-17 11:00:43 -0400

    Update Warsim.java
    edit finaly

--- Mehdi_Java_Assignment_4/Warsim.java
+++ Mehdi_Java_Assignment_4/Warsim.java
@@ -16,25 +16,19 @@ public class Warsim {
   public static Weather weather;
 
   // Player Objects
-  public static Warrior player; // player
-  public static Weapon pWeapon; // player weapon
-  public static Armour pArmour; // player armour
-
-  // Enemy Objects
-  public static Warrior enemy; // enemy
-  public static Weapon eWeapon; // enemy weapon
-  public static Armour eArmour; // enemy armour
+  public static Warrior player1, player2, player3, player4;
+  public static Weapon pWeapon1, pWeapon2, pWeapon3, pWeapon4;
+  public static Armour pArmour1, pArmour2, pArmour3, pArmour4;
 
   // Variables
   public static boolean gameOver = false;
-  public static boolean playerTurn = true;
+  public static int currentTurn = 1;
   public static int choice = 0;
   public static int attackType = 0;
   public static int damage = 0;
-  public static String who = "Player";
   public static String winner = "";
-  public static int winCount = 0; // Initialize winCount to 0
-  public static int lossCount = 0; // Initialize lossCount to 0
+  public static int winCount = 0;
+  public static int lossCount = 0;
 
   public static void main(String[] args) {
     ink.welcomeMessage();
@@ -43,178 +37,181 @@ public class Warsim {
     int weatherType = randNum.nextInt(4) + 1;
     createWeather(weatherType);
 
-    // ====================>>
-    // Player Setup
-    // Warrior
-    ink.printWarriorMenu();
-    int choice = input.nextInt(); // 1, 2 or 3
-    createWarrior(who, choice);
+    // Player 1 Setup
+    System.out.println("Setting up Player 1");
+    setupPlayer("Player 1");
 
-    // Weapon
-    // Weapon
-    ink.printWeaponMenu();
-    System.out.println("4) Magic Staff: Powerful weapon with high damage, but requires special skills to use (Moderate Dexterity Cost).");
-    choice = input.nextInt(); // 1, 2, 3, or 4
-    createWeapon(who, choice);
+    // Player 2 Setup
+    System.out.println("Setting up Player 2");
+    setupPlayer("Player 2");
 
+    // Player 3 Setup
+    System.out.println("Setting up Player 3");
+    setupPlayer("Player 3");
 
-    // Armour
-    ink.printArmourMenu();
-    choice = input.nextInt(); // 1, 2 or 3
-    createArmour(who, choice);
+    // Player 4 Setup
+    System.out.println("Setting up Player 4");
+    setupPlayer("Player 4");
 
-    who = "Enemy"; // swap the who with the what
-
-    // ====================>>
-    // Enemy Setup
-    // Warrior
-    choice = randNum.nextInt(3) + 1;
-    createWarrior(who, choice);
-
-    // Weapon
-    choice = randNum.nextInt(3) + 1;
-    createWeapon(who, choice);
-
-    // Armour
-    choice = randNum.nextInt(3) + 1;
-    createArmour(who, choice);
-
-    ink.printStats(player, enemy);
+    ink.printStats(player1, player2, player3, player4);
 
     // main game loop
-    while (!gameOver) { // while the game is NOT over
-      if (playerTurn) {
-        ink.printAttackMenu();
-        attackType = input.nextInt();
-        damage = pWeapon.strike(weather.getSeverity(), attackType, player.getStrength(), player.getDexterity());
-        damage = eArmour.reduceDamage(damage);
-        enemy.takeDamage(damage);
-        if (!enemy.isAlive()) {
-          winner = "Player";
-          gameOver = !gameOver;
-          winCount++; // Update win count after player wins
-        }
-      } else { // enemy turn code
-        attackType = (int) (Math.floor(Math.random() + 1.5));
-        damage = eWeapon.strike(weather.getSeverity(), attackType, enemy.getStrength(), enemy.getDexterity());
-        damage = pArmour.reduceDamage(damage);
-        player.takeDamage(damage);
-        if (!player.isAlive()) {
-          winner = "Enemy";
-          gameOver = true;
-          lossCount++; // Update loss count after player loses
-        }
+    while (!gameOver) {
+      if (currentTurn == 1) {
+        executeTurn(player1, pWeapon1, pArmour1, "Player 2", player2, pArmour2);
+      } else if (currentTurn == 2) {
+        executeTurn(player2, pWeapon2, pArmour2, "Player 3", player3, pArmour3);
+      } else if (currentTurn == 3) {
+        executeTurn(player3, pWeapon3, pArmour3, "Player 4", player4, pArmour4);
+      } else if (currentTurn == 4) {
+        executeTurn(player4, pWeapon4, pArmour4, "Player 1", player1, pArmour1);
       }
-      ink.printStats(player, enemy);
-      playerTurn = !playerTurn; // toggle turns
-      System.out.println(playerTurn ? "Player's Turn" : "Enemy's Turn");
-    } // while()
+
+      currentTurn = (currentTurn % 4) + 1;
+    }
 
     ink.printGameOver(winner);
-    viewBattleHistory(); // Added line to view battle history
-  } // main()
+    viewBattleHistory();
+  }
 
-  // Helper Methods
+  public static void setupPlayer(String who) {
+    ink.printWarriorMenu();
+    choice = input.nextInt();
+    createWarrior(who, choice);
+
+    ink.printWeaponMenu();
+    choice = input.nextInt();
+    createWeapon(who, choice);
+
+    ink.printArmourMenu();
+    choice = input.nextInt();
+    createArmour(who, choice);
+}
+
+public static void executeTurn(Warrior attacker, Weapon attackerWeapon, Armour attackerArmour, String targetName, Warrior target, Armour targetArmour) {
+  if (gameOver) {
+      System.out.println("The game is over. No further turns can be executed.");
+      return;
+  }
+
+  ink.printAttackMenu();
+  System.out.println("Choose Your Attack!");
+  System.out.println("1) Normal: Standard attack with balanced damage and speed.");
+  System.out.println("2) Heavy: Powerful attack that deals more damage but reduces dexterity for this round.");
+
+  attackType = input.nextInt();
+  damage = attackerWeapon.strike(weather.getSeverity(), attackType, attacker.getStrength(), attacker.getDexterity());
+  damage = targetArmour.reduceDamage(damage);
+  target.takeDamage(damage);
+
+  if (!target.isAlive()) {
+      winner = target.getWarriorType() + " was defeated by " + attacker.getWarriorType();
+      gameOver = true;
+      winCount++;
+      System.out.println(winner);
+  }
+
+  ink.printStats(player1, player2, player3, player4);
+}
+
+
+
   public static void createWarrior(String who, int choice) {
-    if (who.equals("Player")) {
-      switch (choice) {
-        case 1: // Human
-          player = new Human("Human");
-          break;
-        case 2: // Elf
-          player = new Elf("Elf");
-          break;
-        case 3: // Orc
-          player = new Orc("Orc");
-          break;
-        default:
-          System.out.println("oops!");
-          break;
-      } // switch
-    } else {
-      switch (choice) {
-        case 1: // Human
-          enemy = new Human("Human");
-          break;
-        case 2: // Elf
-          enemy = new Elf("Elf");
-          break;
-        case 3: // Orc
-          enemy = new Orc("Orc");
-          break;
-        default:
-          System.out.println("oops!");
-          break;
-      } // switch
+    switch (who) {
+      case "Player 1":
+        player1 = createWarriorByChoice(choice);
+        break;
+      case "Player 2":
+        player2 = createWarriorByChoice(choice);
+        break;
+      case "Player 3":
+        player3 = createWarriorByChoice(choice);
+        break;
+      case "Player 4":
+        player4 = createWarriorByChoice(choice);
+        break;
     }
-  } // createWarrior()
+  }
+
+  public static Warrior createWarriorByChoice(int choice) {
+    switch (choice) {
+      case 1:
+        return new Human("Human");
+      case 2:
+        return new Elf("Elf");
+      case 3:
+        return new Orc("Orc");
+      case 4:
+        return new Dwarf("Dwarf");
+      default:
+        System.out.println("Invalid choice!");
+        return null;
+    }
+  }
 
   public static void createWeapon(String who, int choice) {
+    switch (who) {
+      case "Player 1":
+        pWeapon1 = createWeaponByChoice(choice);
+        break;
+      case "Player 2":
+        pWeapon2 = createWeaponByChoice(choice);
+        break;
+      case "Player 3":
+        pWeapon3 = createWeaponByChoice(choice);
+        break;
+      case "Player 4":
+        pWeapon4 = createWeaponByChoice(choice);
+        break;
+    }
+  }
+
+  public static Weapon createWeaponByChoice(int choice) {
     switch (choice) {
-        case 1: // Dagger
-            if (who.equals("Player")) {
-                pWeapon = new Dagger();
-            } else {
-                eWeapon = new Dagger();
-            }
-            break;
-        case 2: // Sword
-            if (who.equals("Player")) {
-                pWeapon = new Sword();
-            } else {
-                eWeapon = new Sword();
-            }
-            break;
-        case 3: // Axe
-            if (who.equals("Player")) {
-                pWeapon = new Axe();
-            } else {
-                eWeapon = new Axe();
-            }
-            break;
-        case 4: // Magic Staff
-            if (who.equals("Player")) {
-                pWeapon = new MagicStaff();
-            } else {
-                eWeapon = new MagicStaff();
-            }
-            break;
-        default:
-            System.out.println("Invalid choice!");
-            break;
-    } // switch
-} // createWeapon()
-
-
+      case 1:
+        return new Dagger();
+      case 2:
+        return new Sword();
+      case 3:
+        return new Axe();
+      case 4:
+        return new MagicStaff();
+      default:
+        System.out.println("Invalid choice!");
+        return null;
+    }
+  }
 
   public static void createArmour(String who, int choice) {
+    switch (who) {
+      case "Player 1":
+        pArmour1 = createArmourByChoice(choice);
+        break;
+      case "Player 2":
+        pArmour2 = createArmourByChoice(choice);
+        break;
+      case "Player 3":
+        pArmour3 = createArmourByChoice(choice);
+        break;
+      case "Player 4":
+        pArmour4 = createArmourByChoice(choice);
+        break;
+    }
+  }
+
+  public static Armour createArmourByChoice(int choice) {
     switch (choice) {
-      case 1: // Leather
-        if (who.equals("Player")) {
-          pArmour = new Leather();
-        } else {
-          eArmour = new Leather();
-        }
-        break;
-      case 2: // Chainmail
-        if (who.equals("Player")) {
-          pArmour = new Chainmail();
-        } else {
-          eArmour = new Chainmail();
-        }
-        break;
-      case 3: // Platemail
-        if (who.equals("Player")) {
-          pArmour = new Platemail();
-        } else {
-          eArmour = new Platemail();
-        }
-        break;
+      case 1:
+        return new Leather();
+      case 2:
+        return new Chainmail();
+      case 3:
+        return new Platemail();
       default:
-        System.out.println("oops!");
-        break;
-    } // switch
-  } // createArmour()
+        System.out.println("Invalid choice!");
+        return null;
+    }
+  }
 
   public static void viewBattleHistory() {
     System.out.println("Battle History:");
@@ -222,26 +219,23 @@ public class Warsim {
     System.out.println("Losses: " + lossCount);
   }
 
-
   public static void createWeather(int weatherType) {
     switch (weatherType) {
-      case 1: // sun
+      case 1:
         weather = new Sun();
         break;
-      case 2: // rain
+      case 2:
         weather = new Rain();
         break;
-      case 3: // wind
+      case 3:
         weather = new Wind();
         break;
-      case 4: // storm
+      case 4:
         weather = new Storm();
         break;
       default:
         System.out.println("Run!! Godzilla!!!");
         break;
-    } // switch
-  } // createWeather()
-
-  // Method to view battle
+    }
+  }
 }
